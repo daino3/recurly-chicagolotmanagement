657a2684ba2c820ba3168690152096f1fdc6193c
daino3 <dainovu3@gmail.com>
2015-04-03 13:50:40 -0500

    fixing initializer

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,8 +2,12 @@ require 'yaml'
 
 env = ChicagoLotManagement::App.environment.to_s
 
-YAML.load_file("#{APP_ROOT}/config/stripe_secret.yml")[env].each do |key, value|
-  ENV[key] = value
+development_file = "#{APP_ROOT}/config/stripe_secret.yml"
+
+if File.exists?(development_file)
+  YAML.load_file(development_file)[env].each do |key, value|
+    ENV[key] = value
+  end
 end
 
 Stripe.api_key = ENV['STRIPE_SECRET_KEY']
